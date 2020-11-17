50ae812d6ea5997668eeb1fb5872ef9aaa825c3c
liem7758 <0>
2020-11-18 07:55:32 +0800

    jenkins使用allure报告

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # test jenkins auto build
 
 import time
+import pytest
 
 localtime = time.asctime( time.localtime(time.time()) )
 print(localtime)
+work_runner = []
+work_runner.append("-s")
+work_runner.append("--alluredir")
+work_runner.append("report")
+pytest.main(work_runner)
